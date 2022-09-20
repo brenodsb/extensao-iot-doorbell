98e5bd1709b057f36954c7dace742eaeec528ebb
Breno Sousa <90528164+brenodsb@users.noreply.github.com>
2022-09-20 12:17:00 -0300

    Update extensao_iot_doorbell.ino

--- extensao_iot_doorbell.ino
+++ extensao_iot_doorbell.ino
@@ -1,21 +1,18 @@
-int botao = 0;
+int botao = 0; //declaracao de variavel para estado do botao
 
-void setup()
-{
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+void setup(){
+  pinMode(2, INPUT); //pino 2 de entrada (botao)
+  pinMode(13, OUTPUT); //pino 13 de saida (transistor/lampada)
 }
 
-void loop()
-{
-  botao = digitalRead(2);
-  if (botao == LOW) {
-    for (int i = 0; i < 20; i++) 
-	{
-      digitalWrite(13, HIGH);
-      delay(500);
-      digitalWrite(13, LOW);
-      delay(500);
-	}
+void loop(){
+  botao = digitalRead(2); //variavel recebe estado do pino 2
+  if (botao == LOW){//quando pressionado
+    for (int i = 0; i < 20; i++){ //acende e apaga 20x
+      digitalWrite(13, HIGH); //liga
+      delay(500); //espera 0,5 segundos
+      digitalWrite(13, LOW); //desliga
+      delay(500); //espera 0,5 segundos
+    }
   }
 }
